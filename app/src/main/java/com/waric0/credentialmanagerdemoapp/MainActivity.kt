1adcb86e5cf86403342d6dc717fb670b201e9531
waric0 <22999833+waric0@users.noreply.github.com>
2024-09-10 22:09:11 +0900

    Adding Composable Components for Text Fields and Buttons

--- app/src/main/java/com/waric0/credentialmanagerdemoapp/MainActivity.kt
+++ app/src/main/java/com/waric0/credentialmanagerdemoapp/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.autofill.AutofillNode
+import androidx.compose.ui.autofill.AutofillType
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.layout.boundsInWindow
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalAutofill
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.waric0.credentialmanagerdemoapp.ui.theme.CredentialManagerDemoAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +30,93 @@ class MainActivity : ComponentActivity() {
         setContent {
             CredentialManagerDemoAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(top = innerPadding.calculateTopPadding()),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        AutofillUsername()
+                        Spacer(modifier = Modifier.height(20.dp))
+                        AutofillPassword()
+                        Spacer(modifier = Modifier.height(20.dp))
+                        Button(onClick = { }) {
+                            Text("Login")
+                        }
+                    }
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun AutofillUsername() {
+    var username by remember { mutableStateOf("") }
+    val autoFillNode =
+        AutofillNode(autofillTypes = listOf(AutofillType.Username), onFill = { username = it })
+    val autofill = LocalAutofill.current
+    TextField(
+        modifier = Modifier
+            .onGloballyPositioned {
+                autoFillNode.boundingBox = it.boundsInWindow()
+            }
+            .onFocusChanged {
+                if (it.isFocused) {
+                    autofill?.requestAutofillForNode(autoFillNode)
+                } else {
+                    autofill?.cancelAutofillForNode(autoFillNode)
+                }
+            },
+        value = username,
+        onValueChange = { username = it },
+        label = {
+            Text(text = "username")
+        }
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun AutofillUsernamePreview() {
     CredentialManagerDemoAppTheme {
-        Greeting("Android")
+        AutofillUsername()
+    }
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun AutofillPassword() {
+    var password by remember { mutableStateOf("") }
+    val autoFillNode =
+        AutofillNode(autofillTypes = listOf(AutofillType.Password), onFill = { password = it })
+    val autofill = LocalAutofill.current
+    TextField(
+        modifier = Modifier
+            .onGloballyPositioned {
+                autoFillNode.boundingBox = it.boundsInWindow()
+            }
+            .onFocusChanged {
+                if (it.isFocused) {
+                    autofill?.requestAutofillForNode(autoFillNode)
+                } else {
+                    autofill?.cancelAutofillForNode(autoFillNode)
+                }
+            },
+        value = password,
+        onValueChange = { password = it },
+        label = {
+            Text(text = "password")
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AutofillPasswordPreview() {
+    CredentialManagerDemoAppTheme {
+        AutofillPassword()
     }
 }
